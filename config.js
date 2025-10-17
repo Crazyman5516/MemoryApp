5580904173f0e1404400c534d174167ba43d8df1
Tunahan Acar <acartunahan55@gmail.com>
2025-10-17 13:26:30 +0300

    second commit

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ const config = {
     host: process.env.DB_HOST || "localhost",
     user: process.env.DB_USER || "root",
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE || "blogapp",
+    database: process.env.DB_DATABASE || "memoryapp",
   },
   email: {
     username: process.env.EMAIL_USERNAME,
